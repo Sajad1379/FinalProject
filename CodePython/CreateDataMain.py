eaaeb64cb935bc47f3ab70e58af77f43f1259c5b
Sajad1379 <82404707+Sajad1379@users.noreply.github.com>
2023-06-06 02:12:11 +0330

    final change

--- CodePython/CreateDataMain.py
+++ CodePython/CreateDataMain.py
@@ -5,6 +5,9 @@ import openpyxl
 import os
 import time
 
+# Add_laptop = "C:\Users\sajad\Desktop\FinalProject"
+
+# Add_comp = "C:\Users\hp\Desktop\FinalProject"
 
 
 activity_type = [
@@ -16,11 +19,11 @@ activity_type = [
     'subpage' , 'url'    
  ]
 
-studentVle=pd.read_excel(r'C:\Users\hp\Desktop\FinalProject\studentVleTest.xlsx',engine='openpyxl')
+studentVle=pd.read_excel(r'C:\Users\sajad\Desktop\FinalProject\studentVleTest.xlsx',engine='openpyxl')
 
-student=pd.read_excel(r'C:\Users\hp\Desktop\FinalProject\test.xlsx',engine='openpyxl')
+student=pd.read_excel(r'C:\Users\sajad\Desktop\FinalProject\test.xlsx',engine='openpyxl')
 
-dataMain = pd.read_excel(r'C:\Users\hp\Desktop\FinalProject\DataMain.xlsx',engine='openpyxl')
+dataMain = pd.read_excel(r'C:\Users\sajad\Desktop\FinalProject\DataMain.xlsx',engine='openpyxl')
 
 rows_studentVle, cols_studentVle = studentVle.shape
 print(f"Number of rows: {rows_studentVle}")
@@ -53,12 +56,8 @@ for i in range(0,rows_studentVle):
 
 X_studentVle = sorted(weight_studentVle, key=lambda x: x[0])
 
-count = 0
-for i in range(0, 663):
-    if(X_studentVle[i][0] == 6516):
-        count = count + 1 
 
-print(count)
+
 #============================================================
 
 
@@ -67,29 +66,22 @@ start = time.time()
 
 
 
-# for i in range(0 , 1):
-#     dataMain.at[i , 'id_student'] = X_student[i][0]
-#     for j in range(0,len(activity_type)):
-#         sum = 0
-#         count = 0
-#         for k in range(0,len(X_studentVle)):
-#             print ((X_student[i][0]) , (X_studentVle[k][0]))
-#             if(6516 == int(X_studentVle[k][0]) and str(activity_type[j])==str(X_studentVle[k][2])):
-#                 count = count + 1
-#                 sum = sum + int(X_studentVle[k][1])
-#                 del X_studentVle[k]
-#                 print("okkkkkkkkkkkkkkkkkkkkk" , sum)
+for i in range(0 , 1):
+    dataMain.at[i , 'id_student'] = X_student[i][0]
+    for j in range(0,len(activity_type)):
+        sum = 0
+        count = 0
+        for k in range(0,rows_studentVle):
+            
+            if(int(X_student[i][0]) == int(X_studentVle[k][0]) and str(activity_type[j])==str(X_studentVle[k][2])):
                 
-
-#             if(6516 < X_studentVle[k][0]):
-#                 break
-#         print(count)   
-#         dataMain.at[i , activity_type[j]] = sum
+                count = count + 1
+                sum = sum + int(X_studentVle[k][1])
+            
+        dataMain.at[i , activity_type[j]] = sum
     
-#     if i==1000 :
-#         len(X_studentVle)
-#     dataMain.at[i , 'final_result'] = X_student[i][1]
-# end = time.time()
+    dataMain.at[i , 'final_result'] = X_student[i][1]
+end = time.time()
 
-# print(end-start)
-# dataMain.to_excel(r'C:\Users\hp\Desktop\FinalProject\DataMain.xlsx', index=False)
+print(end-start)
+dataMain.to_excel(r'C:\Users\sajad\Desktop\FinalProject\DataMain.xlsx', index=False)
